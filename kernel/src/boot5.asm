f25d6d0be2cb867d7cea83fbf98762e6e17bedf4
OS_AC713 <286123419+OS-AC713@users.noreply.github.com>
2026-08-24 23:30:47 +0400

    link to tutorial :p

--- kernel/src/boot5.asm
+++ kernel/src/boot5.asm
@@ -4,7 +4,7 @@
 ; File name: boot5.asm
 ; License: GNU GPLv3.0 ( see the LICENSE file for details: https://github.com/OS-AC713/os-core713/blob/main/LICENSE )
 ;  WARNING: The file is provided WITHOUT ANY WARRANTY
-;    TO SEE HOW TO CORRECTLY COMPILE A FILE, LOOK AT THE COMMIT ON THIS FILE!
+;    TO SEE HOW TO CORRECTLY COMPILE A FILE, LOOK AT THE COMMIT ON THIS FILE! https://github.com/OS-AC713/os-core713/commit/f1848cb9973b5f515e8efadefe9c8a49ac503e89
 
 bits 16
 ORG 0x7C00
